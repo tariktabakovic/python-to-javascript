ae313fbc29c9920a1533cc2e6aeef6bfdea9e2bf
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2019-12-02 10:01:48 -0500

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+// This is a comment.
+//  this is another comment.
+
+// Variables! (storing single pieces of info)
+// turkey_legs_consumed = 3
+var turkey_legs_consumed = 3;
+//  1. Prefix your variables the first time you use them
+//  2. Use camelCase
+//  3. Terminate each line with ";"
+
+// Var was the original way to declare variables
+// "Let" is better. 
+// 1. It gives you more control. (You can limit it to a specific 
+// is/else or a specific loop.)  
+let turkeysCooked = 2;
+
+// "const" is like "let"(you have control), but:
+// - You must assign it when you declare it.
+// - You can never reassign it  
+const numberOfTurkeysICanEat = 1;
+
+// How to use there keywords:
+// 1. Create your variable using const
+// 2. If you find out you need to reassign it, change it to "let"
+
+// print ("Hello World")
+console.log("Hello World");
+
+// print (turkeysCooked)
+console.log(turkeysCooked);
